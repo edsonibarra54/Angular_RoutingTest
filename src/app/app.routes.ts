5e1fabf188abb826ffa16d619ecfdbd709203179
Edson Ibarra <edsonibarra54@gmail.com>
2024-03-04 17:56:31 -0600

    Added a new page to use when a route is incorrect, also changed the style of the header with the route links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,17 @@ import { HomePage } from './pages/home/home.page';
 import { ContactPage } from './pages/contact/contact.page';
 import { AboutPage } from './pages/about/about.page';
 import { TvShowPage } from './pages/tv-show/tv-show.page';
+import { ErrorPage } from './pages/error/error.page';
 
 export const routes: Routes = [
+    { path: "", redirectTo: "tvShow", pathMatch: "full" },
     { path: "home", component: HomePage },
     { path: "contact", component: ContactPage },
     { path: "about", component: AboutPage },
-    { path: "tvShow", component: TvShowPage }
+    { path: "tvShow", component: TvShowPage },
+    { path: "error", component: ErrorPage },
+    // Option 1
+    //{ path: "**", component: ErrorPage }
+    // Option 2 (better)
+    { path: "**", redirectTo: "error", pathMatch: "full" }
 ];
---
+++ src/app/pages/error/error.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error',
+  standalone: true,
+  imports: [],
+  templateUrl: './error.page.html',
+  styleUrl: './error.page.css'
+})
+export class ErrorPage {
+
+}
